f7ec957d8778e9cc73bb3632635140ee9674f512
DRLOV <phukondurlov45@gmail.com>
2023-09-28 08:06:24 +0530

    complete pattern printing medium level

---
+++ 09_pattern_printing_medium.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int row, col;
+    // for (row = 1; row <= 5; row++)
+    // {
+    //     for (col = 1; col <= row; col++)
+    //     {
+    //         cout << "*"
+    //              << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // int num, col;
+    // for (num = 1; num <= 20; num++)
+    // {
+    //     for (col = 1; col <= num; col++)
+    //     {
+    //         cout << "-"
+    //              << " ";
+    //         /* code */
+    //     }
+    //     cout << endl;
+    // }
+
+    // int num, col;
+    // for (num = 5; num <= 200; num += 5)
+    // {
+    //     for (col = 1; col <= num; col++)
+    //     {
+    //         cout << num
+    //              << " ";
+    //         /* code */
+    //     }
+    //     cout << endl;
+    // }
+
+    // int row, col;
+    // for (row = 1; row <= 5; row++)
+    // {
+    //     for (col = row; col >= 1; col--)
+    //     {
+    //         cout << col << " ";
+    //     }
+
+    //     cout << endl;
+    // }
+
+    // PRINT A TO E EQUAL ROW TIMES
+    /*
+    int row, col;
+    for (row = 1; row <= 5; row++)
+    {
+        char name = 'a' + (row - 1);
+        for (col = 1; col <= row; col++)
+        {
+            cout << name << " ";
+        }
+        cout << endl;
+    }
+    */
+
+    //    PRINT * IN REVERSE ORDER ROW TIMES
+
+    // int row, col;
+    // for (row = 1; row <= 5; row++)
+    // {
+    //     // for (col = 5; col >= row; col--)
+    //     // {
+    //     //     cout << "* ";
+    //     // }
+    //     for (col = 1; col <= 5 - (row - 1); col++)
+    //     {
+    //         cout << "* ";
+    //     }
+
+    //     cout << endl;
+    // }
+
+    // PRINT 1 TO 5 IN REVERSE ORDER
+    // int row, col;
+    // for (row = 5; row >= 1; row--)
+    // {
+    //     for (col = 1; col <= row; col++)
+    //     {
+    //         cout << col << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // for (row = 1; row <= 5; row++)
+    // {
+    //     for (col = 1; col <= 5 - (row - 1); col++)
+    //     {
+    //         cout << col << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // for (row = 1; row <= 5; row++)
+    // {
+    //     for (col = 1; col <= 5 - row + 1; col++)
+    //     {
+    //         cout << col << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // PRINT 5 TO 1
+    int row, col;
+
+    for (row = 1; row <= 5; row++)
+    {
+        // for (col = 5; col >= 5 - row + 1; col--)
+        // {
+        //     cout << col << " ";
+        // }
+        for (col = 5; col >= 5 - row + 1; col--)
+        {
+            cout << col << " ";
+        }
+
+        cout << endl;
+    }
+}
