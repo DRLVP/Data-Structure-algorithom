4072696a3e668f1438dd6550a57049fd8074de67
DRLOV <phukondurlov45@gmail.com>
2023-11-28 17:47:41 +0530

    complete string concept

--- 01_firstProgram.cpp
+++ 01_firstProgram.cpp
@@ -6,9 +6,10 @@ int main()
     // << insertion operator
     // cout << "namste dunya \n";
     // cout << "hello doston \n";
-    int name = 10; // integer
-    // cout << name;
-    char cc = 'a'; // character
+    int num = 10; // integer is 4 bytes (it's means 4 * 8 = 32 bit)
+    // cout << num;
+
+    char cc = 'a'; // character is 1 bytes (it's means 1* 8 = 8bit)
     // cout << cc;
 
     // char word = 'da' // not allowed
@@ -16,19 +17,19 @@ int main()
     // cout << "hello world \n"
     //      << "hello pagolsob";
 
-    long int num = 533323125; // write long number with 64 bit memory
-    // cout << num;
+    // long int num = 533323125; // write long number with 64 bit memory
+    //  cout << num;
 
-    float point = 1.22; // float point datatype
-    // cout << point;
+    // float point = 1.22; // float point datatype 4 bytes (it's means 4 * 8 = 32bit)
+    //  cout << point;
 
-    double _point = 2.55232; // float point with 64bit memory
-    // cout << _point;
+    // double _point = 2.55232; // float point with 64bit memory
+    //  cout << _point;
 
-    bool yes = true;
-    cout << yes << endl;
+    // bool yes = true;  // bool datatype is 1 byte (8 bit)
+    // cout << yes << endl;
 
-    int a, b;
-    cin >> a, b;
-    cout << a + b;
+    // int a, b;
+    // cin >> a, b;
+    // cout << a + b;
 }
---
+++ 50_string.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // char arr[] = {'D', 'U', 'R', 'L', 'O', 'V'};
+    // cout << arr;
+
+    // char arr[20];
+    // cin >> arr;
+    // cout << arr;
+
+    // string s;
+    // cout << "Enter your name: ";
+    // getline(cin, s);
+    // cout << s;
+
+    string myStr = "Hello world!";
+    // cout << myStr.size();
+
+    // concat string in c++
+    string firstName = "Durlov ", lastName = "Phukon";
+    string fullName = firstName.append(lastName);
+    // cout << fullName;
+
+    // escape sequence character
+    string sentence = "welcome to my \" world !\"";
+    // cout << sentence;
+
+    // REVERSE STRING PROBLEM
+    string animal = "tiger";
+}
